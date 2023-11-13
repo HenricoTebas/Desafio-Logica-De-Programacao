283ae56d6c66eef6ee03cd78d33000647bd56854
HenricoTebas <henricotebas@gmail.com>
2023-11-13 18:53:37 -0300

    Projeto Criado

---
+++ Desafio.js
@@ -0,0 +1,63 @@
+let nickName = "Dark Lord"
+let xp = 10.500
+
+if (xp <= 1.000){
+    xp = "Ferro"
+}
+else if (xp >= 1.001 && xp <= 2.000){
+    xp = "Bronze"
+}
+else if (xp >= 2.001 && xp <= 5.000){
+    xp = "Prata"
+}
+else if (xp >= 6.001 && xp <= 7.000){
+    xp = "Ouro"
+}
+else if (xp >= 7.001 && xp <= 8.000){
+    xp = "Platina"
+}
+else if (xp >= 8.001 && xp <= 9.000){
+    xp = "Ascendente"
+}
+else if (xp >= 9.001 && xp <= 10.000){
+    xp = "Imortal"
+}
+else{
+    xp = "Radiante"
+}
+
+console.log(" O Herói de nome " + nickName + " está no nível de " + xp)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Se XP for menor do que 1.000 = Ferro
+Se XP for entre 1.001 e 2.000 = Bronze
+Se XP for entre 2.001 e 5.000 = Prata
+Se XP for entre 6.001 e 7.000 = Ouro
+Se XP for entre 7.001 e 8.000 = Platina
+Se XP for entre 8.001 e 9.000 = Ascendente
+Se XP for entre 9.001 e 10.000= Imortal
+Se XP for maior ou igual a 10.001 = Radiante
+
+## Saída
+
+Ao final deve se exibir uma mensagem:
+"O Herói de nome **{nome}** está no nível de **{nivel}**"  */
